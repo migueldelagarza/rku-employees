e825cf61f2417c67f648bb33804376665c862959
Miguel de la Garza <hola@migueldelagarza.dev>
2023-07-17 17:34:23 -0600

    #3 Se agrega función en controller para buscar empleado por su número

--- controllers/employees.controller.js
+++ controllers/employees.controller.js
@@ -20,6 +20,28 @@ exports.getEmployees = async (_, res) => {
 };
 
 /**
+ * Obtiene un empleado por su número
+ * @param req petición
+ * @param res respuesta
+ */
+exports.getEmployeByAccount = async (req, res) => {
+  try {
+    const { accountNumber } = req.params;
+    console.log(`Consultando usuario número: ${accountNumber}`);
+    const result = await _employes.getEmployeeByAccount(accountNumber)
+      .then( result => {
+        res.status(200).send(result);
+      })
+      .catch( error => {
+        res.send(error.sqlMessage)
+      })
+  } catch (error) {
+    console.error('Error al obtener usuario:', error);
+    res.status(500).send('Error en la solicitud');
+  }
+}
+
+/**
  * Guardo nuevo empleado
  * @param req petición
  * @param res respuesta
@@ -33,9 +55,9 @@ exports.addEmployee = async (req, res) => {
         res.status(201).send('Empleado agregado')
       })
       .catch((error) => {
-      console.error('Error al escribir en base de datos', error);
-      res.status(400).send(error.sqlMessage);
-    });
+        console.error('Error al escribir en base de datos', error);
+        res.status(400).send(error.sqlMessage);
+      });
   } catch (error) {
     console.error('Error al guardar empleado');
     res.status(500).send('Error en servidor/solicitud');
