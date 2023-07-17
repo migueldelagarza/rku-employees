71b191a827c7fe70aee6c207d85b7170a3911bc6
Miguel de la Garza <hola@migueldelagarza.dev>
2023-07-17 15:19:08 -0600

    Se implementa archivo de rutas para gestiòn de usuarios #1

--- main.js
+++ main.js
@@ -5,10 +5,12 @@ const app = express();
 
 const config = require('./config');
 
+const routes = require('./routes/employees.routes');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use(`/`, (_, res) => res.send('works'));
+app.use(`/`, routes);
 
 app.listen(config.PORT, () => {
   console.log(`Microservicio v${config.VERSION} escuchando en el puerto ${config.PORT}`);
